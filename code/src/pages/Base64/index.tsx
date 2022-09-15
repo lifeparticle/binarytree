c81b4882c1cc98ae5200ab6ba66bb63d38771fa1
lifeparticle <mahbubzaman17@gmail.com>
2022-09-15 21:50:53 +1000

    vscode config

--- code/src/pages/Base64/index.tsx
+++ code/src/pages/Base64/index.tsx
@@ -1,59 +1,51 @@
 import { Textarea, SegmentedControl, Button } from "@mantine/core";
 import { useState } from "react";
-import { Value } from "sass";
-
-
-
 
 const Base64: React.FC = () => {
-	const[input, setInput] = useState("");
-	const[result, setResult] = useState("");
-	const [value, setValue] = useState('encode');
+	const [input, setInput] = useState("");
+	const [result, setResult] = useState("");
+	const [value, setValue] = useState("encode");
 	const onButtonClick = () => {
-		if(value === '')
-			return;
-		if(value === 'encode'){
+		if (value === "") return;
+		if (value === "encode") {
 			setResult(btoa(input));
-
-		}else{
+		} else {
 			setResult(atob(input));
 		}
-	}
+	};
 
 	return (
 		<>
-      <Textarea
-	  	value={input}
-		onChange = {(currentValue) => setInput(currentValue.target.value)}
-        placeholder="Input"
-        label="Input"
-        autosize
-      />
+			<Textarea
+				value={input}
+				onChange={(currentValue) => setInput(currentValue.target.value)}
+				placeholder="Input"
+				label="Input"
+				autosize
+			/>
 
-      <Textarea
-	  	value={result}
-		onChange = {(currentValue) => setResult(currentValue.target.value)}
-        label="Result"
-        placeholder="Result"
-        autosize
-        minRows={2}
-      />
+			<Textarea
+				value={result}
+				onChange={(currentValue) =>
+					setResult(currentValue.target.value)
+				}
+				label="Result"
+				placeholder="Result"
+				autosize
+				minRows={2}
+			/>
 
-	<SegmentedControl
-		value={value}
-		onChange={setValue}
-		data={[
-			{ label: 'Encode', value: 'encode' },
-			{ label: 'Decode', value: 'decode' },
-		]}
-    />
+			<SegmentedControl
+				value={value}
+				onChange={setValue}
+				data={[
+					{ label: "Encode", value: "encode" },
+					{ label: "Decode", value: "decode" },
+				]}
+			/>
 
-	<Button onClick={onButtonClick}>
-      Convert
-    </Button>
-    </>
-
-	
+			<Button onClick={onButtonClick}>Convert</Button>
+		</>
 	);
 };
 
