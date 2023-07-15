fd167d2daa5b2c03abe16309817a3706d0aa16a9
Ashik <ashik@pop-os.localdomain>
2023-07-15 17:04:12 +0600

    change sidebar , toogle sidebar & add dark mode & icons
    Former-commit-id: df7b4350f6c13c1f87317890329fdfafd8d09612

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,12 +1,13 @@
-import React, { useState } from "react";
 import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
-import { Layout, Button, theme } from "antd";
+import { Button, ConfigProvider, Layout, theme } from "antd";
+import { useState } from "react";
 
-import Navigation from "./pages/Navigation";
 import "App.scss";
-import { lazy, Suspense } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Moon, Sun } from "lucide-react";
 import Sorting from "pages/Sorting";
+import { Suspense, lazy } from "react";
+import { Route, Routes } from "react-router-dom";
+import Navigation from "./pages/Navigation";
 const ColorPicker = lazy(() => import("pages/ColorPicker"));
 const ImageGeneratorFromColors = lazy(
 	() => import("pages/ImageGeneratorFromColors")
@@ -24,83 +25,139 @@ const TableGenerator = lazy(() => import("pages/MdTableGenerator"));
 const { Header, Sider, Content } = Layout;
 
 function App() {
+	const { defaultAlgorithm, darkAlgorithm } = theme;
+	const [isDarkMode, setIsDarkMode] = useState(false);
+
+	const handleClick = () => {
+		setIsDarkMode((previousValue) => !previousValue);
+	};
+
 	const [collapsed, setCollapsed] = useState(false);
 	const {
 		token: { colorBgContainer },
 	} = theme.useToken();
 
 	return (
-		<Layout style={{ background: colorBgContainer }}>
-			<Sider trigger={null} collapsible collapsed={collapsed}>
-				<div className="demo-logo-vertical" />
-				<Navigation />
-			</Sider>
+		<ConfigProvider
+			theme={{
+				algorithm: isDarkMode ? darkAlgorithm : defaultAlgorithm,
+			}}
+		>
 			<Layout>
-				<Header style={{ padding: 0, background: colorBgContainer }}>
-					<Button
-						type="text"
-						icon={
-							collapsed ? (
-								<MenuUnfoldOutlined />
-							) : (
-								<MenuFoldOutlined />
-							)
-						}
-						onClick={() => setCollapsed(!collapsed)}
-						style={{
-							fontSize: "16px",
-							width: 64,
-							height: 64,
-						}}
-					/>
-				</Header>
-				<Content
+				<Sider
 					style={{
-						margin: "24px 16px",
-						padding: 24,
-						minHeight: 280,
-						background: colorBgContainer,
+						backgroundColor: isDarkMode
+							? "#141414"
+							: colorBgContainer,
 					}}
+					trigger={null}
+					collapsible
+					collapsed={collapsed}
 				>
-					<Suspense fallback={<div>Loading...</div>}>
-						<div>
-							<Routes>
-								<Route
-									path="/"
-									element={<ImageGeneratorFromColors />}
-								/>
-								<Route path="/sorting" element={<Sorting />} />
-								<Route path="/cp" element={<ColorPicker />} />
-								<Route
-									path="/me"
-									element={<MarkdownEditor />}
-								/>
-								<Route path="/te" element={<TextEditor />} />
-								<Route path="/icons" element={<Icons />} />
-								<Route
-									path="/data_gen"
-									element={<DataGenerator />}
-								/>
-								<Route path="/base_64" element={<Base64 />} />
-								<Route path="/shades" element={<Shades />} />
-								<Route
-									path="/pixel_converter"
-									element={<PixelConverter />}
-								/>
-								<Route
-									path="/toc"
-									element={<TableOfContent />}
-								/>
-								<Route
-									path="/md_table_generator"
-									element={<TableGenerator />}
-								/>
-							</Routes>
-						</div>
-					</Suspense>
-				</Content>
+					<div className="demo-logo-vertical" />
+
+					<div style={{ display: "flex", justifyContent: "end" }}>
+						<Button
+							type="text"
+							icon={
+								collapsed ? (
+									<MenuUnfoldOutlined />
+								) : (
+									<MenuFoldOutlined />
+								)
+							}
+							onClick={() => setCollapsed(!collapsed)}
+							style={{
+								fontSize: "16px",
+								marginRight: "10px",
+							}}
+						/>
+					</div>
+
+					<Navigation />
+
+					<div style={{ display: "flex", justifyContent: "end" }}>
+						<Button
+							type="text"
+							icon={
+								collapsed ? (
+									<Sun size={24} />
+								) : (
+									<Moon size={24} />
+								)
+							}
+							onClick={handleClick}
+							style={{
+								fontSize: "16px",
+								marginRight: "10px",
+							}}
+						/>
+					</div>
+				</Sider>
+				<Layout>
+					<Content
+						style={{
+							margin: "24px 16px",
+							padding: 24,
+							minHeight: 280,
+							// background: colorBgContainer,
+						}}
+					>
+						<Suspense fallback={<div>Loading...</div>}>
+							<div>
+								<Routes>
+									<Route
+										path="/"
+										element={<ImageGeneratorFromColors />}
+									/>
+									<Route
+										path="/sorting"
+										element={<Sorting />}
+									/>
+									<Route
+										path="/cp"
+										element={<ColorPicker />}
+									/>
+									<Route
+										path="/me"
+										element={<MarkdownEditor />}
+									/>
+									<Route
+										path="/te"
+										element={<TextEditor />}
+									/>
+									<Route path="/icons" element={<Icons />} />
+									<Route
+										path="/data_gen"
+										element={<DataGenerator />}
+									/>
+									<Route
+										path="/base_64"
+										element={<Base64 />}
+									/>
+									<Route
+										path="/shades"
+										element={<Shades />}
+									/>
+									<Route
+										path="/pixel_converter"
+										element={<PixelConverter />}
+									/>
+									<Route
+										path="/toc"
+										element={<TableOfContent />}
+									/>
+									<Route
+										path="/md_table_generator"
+										element={<TableGenerator />}
+									/>
+								</Routes>
+							</div>
+						</Suspense>
+					</Content>
+				</Layout>
 			</Layout>
-		</Layout>
+		</ConfigProvider>
 	);
 }
 
