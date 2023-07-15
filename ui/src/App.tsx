55340e43a27d5143e3c21c32065bedd693967474
Ashik <ashik@pop-os.localdomain>
2023-07-15 17:17:19 +0600

    toggle theme icon
    Former-commit-id: 833b285ce890a55dba7f5e76a40ee7c436decad7

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -80,7 +80,7 @@ function App() {
 						<Button
 							type="text"
 							icon={
-								collapsed ? (
+								isDarkMode ? (
 									<Sun size={24} />
 								) : (
 									<Moon size={24} />
