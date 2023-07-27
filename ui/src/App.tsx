840930f7ba7df1b9482a1d80f9669abb2acbb62c
Mahbub Zaman <mahbubzaman17@gmail.com>
2023-07-27 21:04:13 +1000

    Update App.tsx

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,12 +1,11 @@
-import { ConfigProvider, Layout } from "antd";
-import PageHeader from "atoms/PageHeader";
-import useDarkMode, { useMenuCollapsed } from "lib/utils/hooks";
+import { ConfigProvider, Layout, Typography } from "antd";
+import useDarkMode, { useMenuCollapsed, usePageTitle } from "lib/utils/hooks";
 import { Suspense } from "react";
-
 import { useRoutes } from "react-router-dom";
 import { routes } from "routes.constant";
 import Header from "sections/Header";
 import Menu from "sections/Menu";
+import { ErrorBoundary } from "react-error-boundary";
 
 const { Sider, Content } = Layout;
 
@@ -21,48 +20,57 @@ function App() {
 	const { collapsed, toggleCollapse } = useMenuCollapsed(
 		menuCollapsedStorageKey
 	);
+	const currentTitle = usePageTitle(routes);
 
 	return (
-		<ConfigProvider
-			theme={{
-				algorithm,
-			}}
-		>
-			<Layout>
-				<Header
-					handleThemeChange={toggleTheme}
-					collapsed={collapsed}
-					handleMenuCollapse={toggleCollapse}
-				/>
-				<Layout hasSider>
-					<Sider
-						trigger={null}
-						collapsible
+		<ErrorBoundary fallback={<p>Something went wrong</p>}>
+			<ConfigProvider
+				theme={{
+					algorithm,
+				}}
+			>
+				<Layout>
+					<Header
+						handleThemeChange={toggleTheme}
 						collapsed={collapsed}
-						style={{
-							overflow: "auto",
-							height: "100vh",
-							position: "sticky",
-							left: 0,
-							top: 64,
-							bottom: 0,
-							backgroundColor: isDarkMode
-								? "rgb(20, 20, 20)"
-								: "rgb(255, 255, 255)",
-						}}
-					>
-						<Menu isDarkMode={isDarkMode} collapsed={collapsed} />
-					</Sider>
-					<Content>
-						<PageHeader />
-
-						<Suspense fallback={<div>Loading...</div>}>
-							{useRoutes(routes)}
-						</Suspense>
-					</Content>
+						handleMenuCollapse={toggleCollapse}
+					/>
+					<Layout hasSider>
+						<Sider
+							trigger={null}
+							collapsible
+							collapsed={collapsed}
+							style={{
+								overflow: "auto",
+								height: "100vh",
+								position: "sticky",
+								left: 0,
+								top: 64,
+								bottom: 0,
+								backgroundColor: isDarkMode
+									? "rgb(20, 20, 20)"
+									: "rgb(255, 255, 255)",
+							}}
+						>
+							<Menu
+								isDarkMode={isDarkMode}
+								collapsed={collapsed}
+							/>
+						</Sider>
+						<Content>
+							<PageHeader />
+							<ErrorBoundary
+								fallback={<p>Something went wrong</p>}
+							>
+								<Suspense fallback={<div>Loading...</div>}>
+									{useRoutes(routes)}
+								</Suspense>
+							</ErrorBoundary>
+						</Content>
+					</Layout>
 				</Layout>
-			</Layout>
-		</ConfigProvider>
+			</ConfigProvider>
+		</ErrorBoundary>
 	);
 }
 
