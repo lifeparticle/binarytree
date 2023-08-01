d6d634c893d2ceac35b2c22f59dd61f768da02a5
ashik-75 <ashikex49@gmail.com>
2023-08-01 18:18:16 +0600

    update

--- ui/src/pages/IconList/index.tsx
+++ ui/src/pages/IconList/index.tsx
@@ -1,17 +1,39 @@
+import { LoadingOutlined } from "@ant-design/icons";
 import List from "components/Hoc/List/List";
-import { iconsList } from "./data";
 import Resource from "components/General/ListItems/Resource/Resource";
+import { useQuery } from "@tanstack/react-query";
+import { Spin } from "antd";
+import { getData } from "api/API";
 
-const IconList: React.FC = () => {
+const URL = `./icons.json`;
+const queryKey = "icons";
+
+function IconList() {
+	const { data, isLoading, isError } = useQuery({
+		queryKey: [queryKey],
+		queryFn: () => {
+			return getData(URL);
+		},
+	});
 	return (
-		<>
-			<List
-				items={iconsList}
-				itemComponent={Resource}
-				resourceName="icons"
-			/>
-		</>
+		<div>
+			{isLoading ? (
+				<Spin
+					indicator={
+						<LoadingOutlined style={{ fontSize: 34 }} spin />
+					}
+				/>
+			) : isError ? (
+				<div>Something wrong</div>
+			) : (
+				<List
+					items={data}
+					resourceName="github"
+					itemComponent={Resource}
+				/>
+			)}
+		</div>
 	);
-};
+}
 
 export default IconList;
--- ui/src/pages/YouTubeList/index.tsx
+++ ui/src/pages/YouTubeList/index.tsx
@@ -1,1 +1,39 @@
-export { default } from "./YouTubeList";
+import { LoadingOutlined } from "@ant-design/icons";
+import List from "components/Hoc/List/List";
+import Resource from "components/General/ListItems/Resource/Resource";
+import { useQuery } from "@tanstack/react-query";
+import { Spin } from "antd";
+import { getData } from "api/API";
+
+const URL = `./youtube.json`;
+const queryKey = "youtube";
+
+function YoutubeList() {
+	const { data, isLoading, isError } = useQuery({
+		queryKey: [queryKey],
+		queryFn: () => {
+			return getData(URL);
+		},
+	});
+	return (
+		<div>
+			{isLoading ? (
+				<Spin
+					indicator={
+						<LoadingOutlined style={{ fontSize: 34 }} spin />
+					}
+				/>
+			) : isError ? (
+				<div>Something wrong</div>
+			) : (
+				<List
+					items={data}
+					resourceName="youtube"
+					itemComponent={Resource}
+				/>
+			)}
+		</div>
+	);
+}
+
+export default YoutubeList;
