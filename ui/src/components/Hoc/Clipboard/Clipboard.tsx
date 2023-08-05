15d832c24f4239c2ca44469c47a71e7dcd2129e8
lifeparticle <mahbubzaman17@gmail.com>
2023-08-05 21:53:15 +1000

    list refactor

--- ui/src/components/Hoc/Clipboard/Clipboard.tsx
+++ ui/src/components/Hoc/Clipboard/Clipboard.tsx
@@ -3,23 +3,25 @@ import useCopyToClipboard from "lib/utils/hooks/useCopyToClipboard";
 export interface ClipboardProps {
 	text: string;
 	clipboardComponent: React.ElementType;
+	className?: string;
 }
 
 const Clipboard: React.FC<ClipboardProps> = ({
 	text,
 	clipboardComponent: ClipboardComponent,
+	className = "",
 }) => {
 	const { copied, copyToClipboard, ClipboardMessage } = useCopyToClipboard();
 
 	return (
-		<>
+		<div className={className}>
 			{ClipboardMessage}
 			<ClipboardComponent
 				copyToClipboard={copyToClipboard}
 				copied={copied}
 				text={text}
 			/>
-		</>
+		</div>
 	);
 };
 
