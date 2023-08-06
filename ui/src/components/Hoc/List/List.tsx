f6dd818369aa2263968c675ee9496c43a7ccbe51
ashik-75 <ashikex49@gmail.com>
2023-08-06 16:04:18 +0600

    change list view & category filter

--- ui/src/components/Hoc/List/List.tsx
+++ ui/src/components/Hoc/List/List.tsx
@@ -1,6 +1,6 @@
 import { ChangeEvent, useState } from "react";
 import { useSearchParams } from "react-router-dom";
-import { Input } from "antd";
+import { Input, Tag } from "antd";
 import style from "./list.module.scss";
 import { ListProps } from "./types";
 import SkeletonCard from "components/General/ListItems/SkeletonCard";
@@ -14,12 +14,18 @@ const filteredNews = <T,>(searchQuery: string, items: T[]) => {
 	return items;
 };
 
-const filteredResource = <T,>(searchQuery: string, items: T[]) => {
-	return items?.filter((listItem) =>
-		listItem.subCategory.some((subcategory) =>
-			subcategory.toLowerCase().includes(searchQuery.toLowerCase())
-		)
-	);
+const filteredResource = <T,>(type: string, value: string, items: T[]) => {
+	if (type === "text") {
+		return items.filter((item) =>
+			item?.name.toLowerCase().includes(value.toLowerCase())
+		);
+	} else if (type === "cat") {
+		return items.filter((item) =>
+			item?.category.toLowerCase().includes(value.toLowerCase())
+		);
+	} else {
+		return items;
+	}
 };
 
 const List = <T,>({
@@ -31,7 +37,9 @@ const List = <T,>({
 }: ListProps<T>): JSX.Element => {
 	const [searchParams, setSearchParams] = useSearchParams();
 	const query = searchParams.get("q") || "";
+	const category = searchParams.get("cat") || "";
 	const [searchQuery, setSearchQuery] = useState(query);
+	const [categoryQuery, setCategoryQuery] = useState(category);
 
 	const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
 		const value = e.target.value;
@@ -39,24 +47,60 @@ const List = <T,>({
 		setSearchParams(`?q=${value}`);
 	};
 
+	const handleCategoryChange = (value: string) => {
+		setCategoryQuery(value);
+		setSearchParams(`?cat=${value}`);
+	};
+
 	const filteredList =
 		resourceName === "news"
 			? filteredNews(searchQuery, items)
-			: filteredResource(searchQuery, items);
+			: filteredResource(
+					searchQuery ? "text" : categoryQuery ? "cat" : "",
+					searchQuery || categoryQuery,
+					items
+			  );
 
 	const handleOnClick = (url: string) => {
 		window.open(url, "_blank");
 	};
 
+	const subCategorys =
+		resourceName !== "news"
+			? [...new Set(items?.map((item) => item?.category))]
+			: [];
+
 	return (
 		<div className={style.container}>
 			<Input
 				type="text"
-				placeholder="Search..."
+				placeholder="Search by title..."
 				value={searchQuery}
 				onChange={handleSearchChange}
-				className={style.container_input}
 			/>
+
+			{subCategorys.length > 0 && (
+				<div>
+					<Tag
+						color={category === "" ? "cyan" : "default"}
+						onClick={() => handleCategoryChange("")}
+						className={style.container__tag}
+					>
+						All
+					</Tag>
+					{subCategorys?.map((cat) => (
+						<Tag
+							onClick={() => handleCategoryChange(cat)}
+							key={cat}
+							color={category === cat ? "cyan" : "default"}
+							className={style.container__tag}
+						>
+							{cat}
+						</Tag>
+					))}
+				</div>
+			)}
+
 			{isLoading ? (
 				<SkeletonCard />
 			) : isError ? (
