f01e5feaa4f1c422e1eb24b234fb9fc9484ef217
lifeparticle <mahbubzaman17@gmail.com>
2023-08-07 22:51:56 +1000

    refactor

--- ui/src/components/General/ListItems/Resource/Resource.tsx
+++ ui/src/components/General/ListItems/Resource/Resource.tsx
@@ -1,7 +1,7 @@
 import { Avatar, Card, Skeleton, Space, Tag, Typography } from "antd";
 import style from "./resource.module.scss";
-import { ListItemProps } from "components/Hoc/List/types";
 import { ResourceType } from "./resource.type";
+import { ListItemProps } from "components/Hoc/List/types";
 import Clipboard from "components/Hoc/Clipboard/Clipboard";
 import ClipboardButton from "components/General/ClipboardButton/ClipboardButton";
 
@@ -13,15 +13,13 @@ const Resource: React.FC<ListItemProps<ResourceType>> = ({
 }) => {
 	const { name, category, url } = resource || {};
 
-	const FIRST_LETTER = name?.[0];
-
 	return (
 		<Card className={style.card} key={name} hoverable>
 			<Skeleton loading={isLoading} avatar active>
 				<Space className={style.card__container}>
 					<Space size={16}>
 						<Avatar className={style.card__avatar}>
-							{FIRST_LETTER}
+							{name?.[0]}
 						</Avatar>
 						<Space direction="vertical">
 							<Title level={4}>{name}</Title>
