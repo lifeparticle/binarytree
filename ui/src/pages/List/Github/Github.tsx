b374a48f794db2ce6f83502887ee5fae552e1a10
lifeparticle <mahbubzaman17@gmail.com>
2023-08-08 00:27:50 +1000

    refactor

--- ui/src/pages/List/Github/Github.tsx
+++ ui/src/pages/List/Github/Github.tsx
@@ -2,15 +2,16 @@ import List from "components/Hoc/List/List";
 import Resource from "components/General/ListItems/Resource/Resource";
 import useFetchList from "lib/utils/hooks/useFetchList";
 
-export const URL = `./icons.json`;
-export const QUERY_KEY = "icons";
+const URL = `./github.json`;
+const QUERY_KEY = "github";
 
-const IconList: React.FC = () => {
+const Github: React.FC = () => {
 	const { data, isLoading, isError } = useFetchList(QUERY_KEY, URL);
+
 	return (
 		<List
 			items={data}
-			resourceName="github"
+			resourceName={QUERY_KEY}
 			itemComponent={Resource}
 			isLoading={isLoading}
 			isError={isError}
@@ -18,4 +19,4 @@ const IconList: React.FC = () => {
 	);
 };
 
-export default IconList;
+export default Github;
