635ac5a9987b82912ce7feec20f37b818a02290b
ashik-75 <ashikex49@gmail.com>
2023-08-08 15:09:49 +0600

    handle mode with context
    Former-commit-id: d6821fa073f976ed154c7c7e2e3ed84ea5ce4272

--- ui/src/lib/utils/hooks/useDarkMode.ts
+++ ui/src/lib/utils/hooks/useDarkMode.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { getLocalstorageValue, setLocalstorageValue } from "../helper";
 import { theme } from "antd";
 
-const useDarkMode = (storageKey: string, defaultValue: boolean) => {
+const useDarkMode = (storageKey: string, defaultValue = false) => {
 	const { defaultAlgorithm, darkAlgorithm } = theme;
 	const [isDarkMode, setIsDarkMode] = useState(() => {
 		return getLocalstorageValue<boolean>(storageKey) ?? defaultValue;
