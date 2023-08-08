6ff2a0270d1e0411774f2ee42094ee5b32eca9fc
Mahbub Zaman <mahbubzaman17@gmail.com>
2023-08-09 00:47:02 +1000

    Update Tool.tsx
    Former-commit-id: 46add633ccd0ed54a7100b98b309c9ef5913234b

--- ui/src/pages/List/Tool/Tool.tsx
+++ ui/src/pages/List/Tool/Tool.tsx
@@ -1,1 +1,22 @@
-//todo
+import List from "components/Hoc/List/List";
+import Resource from "components/General/ListItems/Resource/Resource";
+import useFetchList from "lib/utils/hooks/useFetchList";
+
+const URL = `./tool.json`;
+const QUERY_KEY = "tool";
+
+const Tool: React.FC = () => {
+	const { data, isLoading, isError } = useFetchList(QUERY_KEY, URL);
+
+	return (
+		<List
+			items={data}
+			resourceName={QUERY_KEY}
+			itemComponent={Resource}
+			isLoading={isLoading}
+			isError={isError}
+		/>
+	);
+};
+
+export default Tool;
