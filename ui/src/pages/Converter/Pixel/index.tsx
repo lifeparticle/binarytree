a7adf88d20024f584c423ab833240e4047194352
ashik-75 <ashikex49@gmail.com>
2023-08-19 13:21:18 +0600

    update changes

--- ui/src/pages/Converter/Pixel/index.tsx
+++ ui/src/pages/Converter/Pixel/index.tsx
@@ -1,97 +1,84 @@
 import { useState } from "react";
 import style from "./Pixel.module.scss";
-import { InputNumber, Form, Row, Col } from "antd";
+import { Card, Form } from "antd";
+import InputComponent from "components/General/InputComponent";
+import CopyInput from "components/Layouts/CopyInput";
 import Clipboard from "components/RenderProps/Clipboard";
 import ClipboardButton from "components/General/ClipboardButton";
 
 const Pixel: React.FC = () => {
-	const [pixel, setPixel] = useState(0.0);
-	const [rem, setRem] = useState(0.0);
+	const [pixel, setPixel] = useState(0);
+	const [rem, setRem] = useState(0);
 	const [base, setBase] = useState(16);
 	return (
-		<div className={style.pc}>
+		<Card bordered={false} className={style.pc}>
 			<Form layout="vertical">
-				<Form.Item label="Pixels" tooltip="This is a required field">
-					<Row gutter={[16, 0]}>
-						<Col span={22}>
-							<InputNumber
-								style={{
-									width: "100%",
-								}}
-								precision={2}
-								min={0}
-								step={1}
-								placeholder="Enter pixel value"
-								value={pixel}
-								onChange={(px: number | null) => {
-									if (px) {
-										setPixel(px);
-										setRem(px / base);
-									}
-								}}
-							/>
-						</Col>
-						<Col span={2}>
-							<Clipboard
-								text={`${pixel}`}
-								clipboardComponent={ClipboardButton}
-							/>
-						</Col>
-					</Row>
-				</Form.Item>
+				<CopyInput>
+					<InputComponent
+						label="Pixels"
+						tooltip="This is a required field"
+						value={pixel}
+						onChange={(px: number | null) => {
+							if (px) {
+								setPixel(px);
+								setRem(px / base);
+							} else {
+								setPixel(0);
+								setRem(0);
+							}
+						}}
+						precision={2}
+						min={0}
+						type="number"
+					/>
+					<Clipboard
+						text={`${pixel}`}
+						clipboardComponent={ClipboardButton}
+					/>
+				</CopyInput>
 
-				<Form.Item label="REM" tooltip="This is a required field">
-					<Row gutter={[16, 0]}>
-						<Col span={22}>
-							<InputNumber
-								style={{ width: "100%" }}
-								precision={2}
-								min={0}
-								step={0.01}
-								placeholder="Enter REM value"
-								value={rem}
-								onChange={(rem: number | null) => {
-									if (rem) {
-										setRem(rem);
-										setPixel(rem * base);
-									}
-								}}
-							/>
-						</Col>
-						<Col span={2}>
-							<Clipboard
-								text={`${rem}`}
-								clipboardComponent={ClipboardButton}
-							/>
-						</Col>
-					</Row>
-				</Form.Item>
-				<Form.Item
-					label="Base Font Size"
-					tooltip="This is a required field"
-				>
-					<Row gutter={[16, 0]}>
-						<Col span={22}>
-							<InputNumber
-								style={{ width: "100%" }}
-								className={style.pc__base}
-								precision={2}
-								min={0}
-								step={1}
-								placeholder="Enter Unit"
-								value={base}
-								onChange={(base: number | null) => {
-									if (base) {
-										setBase(base);
-										setRem(pixel / base);
-									}
-								}}
-							/>
-						</Col>
-					</Row>
-				</Form.Item>
+				<CopyInput>
+					<InputComponent
+						label="REM"
+						tooltip="This is a required field"
+						value={rem}
+						onChange={(rem: number | null) => {
+							if (rem) {
+								setRem(rem);
+								setPixel(rem * base);
+							} else {
+								setRem(0);
+								setPixel(0);
+							}
+						}}
+						precision={2}
+						min={0}
+						type="number"
+					/>
+					<Clipboard
+						text={`${pixel}`}
+						clipboardComponent={ClipboardButton}
+					/>
+				</CopyInput>
+
+				<CopyInput>
+					<InputComponent
+						label="Base Font Size"
+						tooltip="This is a required field"
+						value={base}
+						onChange={(base: number | null) => {
+							if (base) {
+								setBase(base);
+								setRem(pixel / base);
+							}
+						}}
+						precision={2}
+						min={0}
+						type="number"
+					/>
+				</CopyInput>
 			</Form>
-		</div>
+		</Card>
 	);
 };
 
