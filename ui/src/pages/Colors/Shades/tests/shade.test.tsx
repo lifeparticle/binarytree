ffb4b1c296315f8f7cfa049a46807b4d6041fa7e
ashik-75 <ashikex49@gmail.com>
2023-08-19 09:38:56 +0600

    Fix eslint error
    Former-commit-id: e7223540ae1d224b2eb2e7bce3844859886cba4f

--- ui/src/pages/Colors/Shades/tests/shade.test.tsx
+++ ui/src/pages/Colors/Shades/tests/shade.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { describe, test } from "vitest";
 import Shades from "..";
 
