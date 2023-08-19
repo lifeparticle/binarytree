00c0a723c81127400fa5d9e1d08cac22d7a57618
ashik-75 <ashikex49@gmail.com>
2023-08-19 13:21:18 +0600

    update changes
    Former-commit-id: a7adf88d20024f584c423ab833240e4047194352

--- ui/src/components/General/InputComponent/index.tsx
+++ ui/src/components/General/InputComponent/index.tsx
@@ -1,36 +1,23 @@
-import { InputNumber, Form, Row, Col, Input } from "antd";
+import { Input, InputNumber } from "antd";
 
 interface InputComponentProps {
 	label: string;
 	precision?: number;
 	min?: number;
+	max?: number;
 	step?: number;
 	value: number | string;
-	onChange: () => void;
 	tooltip?: string;
-	showClipboard?: boolean;
+	onChange: (value: any) => void;
 	placeholder?: string;
-	type?: "text" | "number";
+	type: "text" | "number";
 }
-const InputComponent: React.FC<InputComponentProps> = (props) => (
-	<Form.Item
-		label={props.label}
-		tooltip={props.tooltip || "This is a required field"}
-	>
-		<Row gutter={[16, 0]}>
-			<Col span={22}>
-				{props.type === "number" ? (
-					<InputNumber
-						size="large"
-						style={{ width: "100%" }}
-						{...props}
-					/>
-				) : (
-					<Input size="large" style={{ width: "100%" }} {...props} />
-				)}
-			</Col>
-		</Row>
-	</Form.Item>
-);
+const InputComponent: React.FC<InputComponentProps> = (props) => {
+	return props.type === "number" ? (
+		<InputNumber size="large" style={{ width: "100%" }} {...props} />
+	) : (
+		<Input allowClear size="large" style={{ width: "100%" }} {...props} />
+	);
+};
 
 export default InputComponent;
---
+++ ui/src/components/Layouts/CopyInput/index.tsx
@@ -0,0 +1,26 @@
+import { Row, Col, Form } from "antd";
+import React from "react";
+
+interface CopyInputProps {
+	children: React.ReactNode;
+}
+
+const CopyInput: React.FC<CopyInputProps> = ({ children }) => {
+	const [children1, children2] = React.Children.toArray(children);
+
+	const firstChildProps = (children1 as React.ReactElement).props;
+
+	return (
+		<Form.Item
+			label={firstChildProps.label}
+			tooltip={firstChildProps.tooltip}
+		>
+			<Row gutter={[16, 0]}>
+				<Col span={22}>{children1}</Col>
+				<Col span={2}>{children2}</Col>
+			</Row>
+		</Form.Item>
+	);
+};
+
+export default CopyInput;
