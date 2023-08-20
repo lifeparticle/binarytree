f86a7d689f8747c1976c8e4f96c572e64273a5a7
ashik-75 <ashikex49@gmail.com>
2023-08-20 15:56:07 +0600

    fix height in rich-editor & disable grammerly

--- ui/src/pages/Text/TextEditor/index.tsx
+++ ui/src/pages/Text/TextEditor/index.tsx
@@ -1,8 +1,9 @@
-import { Button } from "antd";
+import { Button, Card } from "antd";
 import { useEffect, useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import style from "./TextEditor.module.scss";
+import "./quill.css";
 
 const TextEditor: React.FC = () => {
 	const [value, onChange] = useState("");
@@ -11,6 +12,7 @@ const TextEditor: React.FC = () => {
 	const [charCountWithoutSpace, setCharCountWithoutSpace] = useState(0);
 
 	useEffect(() => {
+		console.log({ value });
 		const cleanValue = value.replace(/(<([^>]+)>)/gi, " ").trim();
 		if (cleanValue === "") {
 			setWordCount(0);
@@ -18,28 +20,34 @@ const TextEditor: React.FC = () => {
 			setCharCountWithoutSpace(0);
 			return;
 		}
+
+		console.log({ cleanValue });
 		setWordCount(cleanValue.split(/[\s]+/g).length);
-		setCharCount(cleanValue.length);
+		setCharCount(cleanValue.replace(/[\s]+/g, " ").length);
 		setCharCountWithoutSpace(cleanValue.replace(/[\s]+/g, "").length);
 	}, [value]);
 
 	return (
-		<div className={style.te}>
-			<ReactQuill theme="snow" value={value} onChange={onChange} />
-			<div className={style.te__footer}>
-				<h4>Word count: {wordCount}</h4>
-				<h4>Character count: {charCount}</h4>
-				<h4>Character count without space: {charCountWithoutSpace}</h4>
-			</div>
+		<Card>
+			<div className={style.te}>
+				<ReactQuill theme="snow" value={value} onChange={onChange} />
+				<div className={style.te__footer}>
+					<h4>Word count: {wordCount}</h4>
+					<h4>Character count with space: {charCount}</h4>
+					<h4>
+						Character count without space: {charCountWithoutSpace}
+					</h4>
+				</div>
 
-			<Button
-				onClick={() => {
-					onChange("");
-				}}
-			>
-				Clear
-			</Button>
-		</div>
+				<Button
+					onClick={() => {
+						onChange("");
+					}}
+				>
+					Clear
+				</Button>
+			</div>
+		</Card>
 	);
 };
 
