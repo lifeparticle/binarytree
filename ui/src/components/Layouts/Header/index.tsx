618a6db02ca32ad82eeb73b2d7925a36b5c74e96
lifeparticle <mahbubzaman17@gmail.com>
2023-08-25 23:47:38 +1000

    minor css fix

--- ui/src/components/Layouts/Header/index.tsx
+++ ui/src/components/Layouts/Header/index.tsx
@@ -2,16 +2,24 @@ import { Button, Space, theme } from "antd";
 import { Hexagon } from "lucide-react";
 import { Link } from "react-router-dom";
 import style from "./header.module.scss";
+import { useContext } from "react";
+import { DarkModeContext } from "lib/utils/context/DarkModeProvider";
+import { classNames } from "lib/utils/helper";
 
 const Header: React.FC = () => {
 	const {
 		token: { colorBgContainer, colorText },
 	} = theme.useToken();
 
+	const { isDarkMode } = useContext(DarkModeContext);
+
 	return (
 		<Space
 			style={{ backgroundColor: colorBgContainer, width: "100%" }}
-			className={style.header}
+			className={classNames(
+				style.header,
+				isDarkMode ? style.header_dark : style.header_light
+			)}
 		>
 			<Button type="text">
 				<Link to={"/"}>
