4ed708cd8d15a7c3662f2a6afc8cf3b1ab807b96
lifeparticle <mahbubzaman17@gmail.com>
2023-08-26 17:09:42 +1000

    tidy

--- ui/src/pages/List/Icon/index.tsx
+++ ui/src/pages/List/Icon/index.tsx
@@ -1,14 +1,14 @@
 import Resource from "components/General/ListItems/Resource/Resource";
 import useFetchList from "lib/utils/hooks/useFetchList";
 import ListSearchResults from "components/RenderProps/ListSearchResults";
-import { QUERY_KEY, URL } from "./utils/contant";
+import { QUERY_KEY, URL } from "./utils/constants";
 
 const Icon: React.FC = () => {
 	const { data, isLoading, isError } = useFetchList(QUERY_KEY, URL);
 	return (
 		<ListSearchResults
 			items={data}
-			resourceName="github"
+			resourceName={QUERY_KEY}
 			itemComponent={Resource}
 			isLoading={isLoading}
 			isError={isError}
--- ui/src/pages/List/Icon/tests/icon.test.tsx
+++ ui/src/pages/List/Icon/tests/icon.test.tsx
@@ -2,7 +2,7 @@ import { renderHook, waitFor } from "@testing-library/react";
 import useFetchList from "lib/utils/hooks/useFetchList";
 import { describe, test } from "vitest";
 import { wrapper } from "test/wrapper";
-import { QUERY_KEY, URL } from "pages/List/Icon/utils/contant";
+import { QUERY_KEY, URL } from "pages/List/Icon/utils/constants";
 
 describe("Icon List component check", () => {
 	test("Test async call", async () => {
