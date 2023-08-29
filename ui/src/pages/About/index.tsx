aad953b56f4433466786c2978332e4b36195186d
lifeparticle <mahbubzaman17@gmail.com>
2023-08-29 23:59:51 +1000

    fix about page
    Former-commit-id: 7fa814edd49792c271e7bbbd41901a4db99cd828

--- ui/src/pages/About/index.tsx
+++ ui/src/pages/About/index.tsx
@@ -1,42 +1,72 @@
 import { API_COLUMNS, API_DATA, DATA_COLUMNS, data } from "./utils/constants";
-import { Button, Space, Table, Typography } from "antd";
+import { Button, Card, Space, Table, Typography } from "antd";
 import style from "./About.module.scss";
 import Icon from "components/General/Icon";
+import about from "assets/about.jpg";
 
 const About = () => {
 	return (
 		<Space direction="vertical" className={style.about}>
-			<Typography.Title level={3}>Binary Tree</Typography.Title>
-
-			<Typography.Paragraph>
-				binarytree.dev offers a range of developer productivity tools to
-				save time.
-			</Typography.Paragraph>
-
-			<Table
-				columns={API_COLUMNS}
-				dataSource={API_DATA}
-				pagination={false}
-				title={() => "API's"}
-			/>
-			<Table
-				columns={DATA_COLUMNS}
-				dataSource={data}
-				pagination={false}
-				title={() => "Features"}
-			/>
-			<Button
-				type="primary"
-				shape="circle"
-				icon={<Icon name="Github" />}
-				onClick={() =>
-					window.open(
-						"https://github.com/lifeparticle/binarytree",
-						"_blank",
-						"noopener"
-					)
+			<Card
+				style={{ maxWidth: 800 }}
+				title="Binary Tree"
+				bordered={false}
+				cover={
+					<img
+						alt="about"
+						src={about}
+						onClick={() =>
+							window.open(
+								"https://unsplash.com/photos/AaqI2ao96KM",
+								"_blank"
+							)
+						}
+					/>
 				}
-			/>
+			>
+				<Space direction="vertical" size="middle">
+					<Typography.Paragraph>
+						At binarytree.dev, we provide an array of developer
+						productivity tools designed to help you save time. With
+						an ever-growing number of features (currently{" "}
+						<b>{data.length}</b>), our platform is constantly
+						evolving to meet the needs of developers like you.
+					</Typography.Paragraph>
+
+					<Table
+						columns={DATA_COLUMNS}
+						dataSource={data}
+						pagination={false}
+						title={() => "Features"}
+						bordered
+						scroll={{ x: "calc(50%)", y: 800 }}
+					/>
+
+					<Table
+						columns={API_COLUMNS}
+						dataSource={API_DATA}
+						pagination={false}
+						title={() => "API's"}
+						bordered
+					/>
+					<div className={style.about__socials}>
+						<Button
+							className={style.about__socials_button}
+							key="Github"
+							type="primary"
+							shape="circle"
+							icon={<Icon name="Github" />}
+							onClick={() =>
+								window.open(
+									"https://github.com/lifeparticle/binarytree",
+									"_blank",
+									"noopener"
+								)
+							}
+						/>
+					</div>
+				</Space>
+			</Card>
 		</Space>
 	);
 };
