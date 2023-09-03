2ed5d258b067417a2a719e129a2a7b2d6326af95
ashik-75 <ashikex49@gmail.com>
2023-09-03 17:08:26 +0600

    enhance notification and add features
    Former-commit-id: 182bb0e2879c1141da4d3a57cf7dcbaaffaf7f2b

--- ui/src/components/Layouts/FloatingHeader/components/Notification.tsx
+++ ui/src/components/Layouts/FloatingHeader/components/Notification.tsx
@@ -1,62 +1,25 @@
-import React, { useState, useEffect } from "react";
-import style from "./Notification.module.scss";
-import { Markdown } from "components/Layouts/FloatingHeader/utils/types";
-import { parsedMarkdown } from "components/Layouts/FloatingHeader/utils/helper";
-import { DEFAULT_RECORD } from "components/Layouts/FloatingHeader/utils/constants";
-import { Typography, Tag } from "antd";
+import React from "react";
+import {
+	NOTIFICATION_KEY,
+	NOTIFICATION_URL,
+} from "components/Layouts/FloatingHeader/utils/constants";
+import useGetNotifications from "lib/utils/hooks/useGetNotifications";
+import NotificationList from "./NotificationList";
+import { NotificationPropsType } from "../utils/types";
 
-const { Title } = Typography;
-
-const Notification: React.FC = () => {
-	const [notifications, setNotifications] = useState<Markdown[]>([]);
-
-	useEffect(() => {
-		async function fetchChangelog() {
-			try {
-				const response = await fetch("/CHANGELOG.md");
-				const content = await response.text();
-
-				const entries = parsedMarkdown(content);
-
-				setNotifications(entries);
-			} catch (error) {
-				console.error("Error fetching changelog:", error);
-			}
-		}
-
-		if (import.meta.env.MODE === "development") {
-			setNotifications(DEFAULT_RECORD);
-		} else {
-			fetchChangelog();
-		}
-	}, []);
+const Notification: React.FC<NotificationPropsType> = ({ colorText }) => {
+	const { notifications, isLoading, isError } = useGetNotifications(
+		NOTIFICATION_KEY,
+		NOTIFICATION_URL
+	);
 
 	return (
-		<div className={style.notification}>
-			{notifications.map((notification) => (
-				<div
-					key={notification.date}
-					className={style.notification__item}
-				>
-					<div className={style.notification__item_title}>
-						<Title level={5}>{notification.date}</Title>
-						<Tag color="green">{notification.version}</Tag>
-					</div>
-					<ul className={style.notification__item_features}>
-						{notification.features.map((feature) => (
-							<li
-								key={feature}
-								className={
-									style.notification__item_features_feature
-								}
-							>
-								{feature}
-							</li>
-						))}
-					</ul>
-				</div>
-			))}
-		</div>
+		<NotificationList
+			notifications={notifications}
+			colorText={colorText}
+			isLoading={isLoading}
+			isError={isError}
+		/>
 	);
 };
 
