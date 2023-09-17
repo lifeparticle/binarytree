2490693dacfaaa5fe1c6eebe5cd5f33dd243d187
lifeparticle <mahbubzaman17@gmail.com>
2023-09-17 22:38:15 +1000

    refactor

--- ui/src/pages/Information/Mimetype/index.tsx
+++ ui/src/pages/Information/Mimetype/index.tsx
@@ -6,7 +6,9 @@ import MimeSearchResult from "./components/MimeSearchResult";
 const Mimetype: React.FC = () => {
 	const { data, isError, isLoading } = useFetchList(MIME_KEY, MIME_URL);
 
-	return <MimeSearchResult isError={isError} isLoading={true} data={data} />;
+	return (
+		<MimeSearchResult isError={isError} isLoading={isLoading} data={data} />
+	);
 };
 
 export default Mimetype;
