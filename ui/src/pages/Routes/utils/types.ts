0516de3255382b0996d3ad4cb146c4d2712d6286
lifeparticle <mahbubzaman17@gmail.com>
2023-09-18 20:50:21 +1000

    refactor

--- ui/src/pages/Routes/utils/types.ts
+++ ui/src/pages/Routes/utils/types.ts
@@ -1,8 +1,47 @@
+type RouteId =
+	| "newsfeed"
+	| "colorpicker"
+	| "shadesandtints"
+	| "borderradius"
+	| "boxshadow"
+	| "svgformatter"
+	| "base64"
+	| "pixelconverter"
+	| "jsontotypescript"
+	| "data"
+	| "avatar"
+	| "image"
+	| "qrcode"
+	| "sorting"
+	| "blog"
+	| "book"
+	| "course"
+	| "designsystem"
+	| "github"
+	| "icon"
+	| "movie"
+	| "platform"
+	| "plugin"
+	| "tool"
+	| "tvseries"
+	| "uiux"
+	| "youtube"
+	| "editor"
+	| "table"
+	| "tableofcontent"
+	| "texteditor"
+	| "mimetype"
+	| "/"
+	| "about"
+	| "feedback"
+	| "pagenotfound";
+
 interface Route {
 	path: string;
 	title: string;
 	description: string;
 	component: React.FC;
+	id: RouteId;
 }
 
-export type { Route };
+export type { Route, RouteId };
