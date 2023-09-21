b8644f6120519e24d352690fbe7228d5ac473f38
ashik-75 <ashikex49@gmail.com>
2023-09-21 11:03:57 +0600

    update url for safety
    Former-commit-id: 40810f7810247f3848d8c4950978bcec8300f187

--- ui/src/pages/Newsfeed/utils/hooks.ts
+++ ui/src/pages/Newsfeed/utils/hooks.ts
@@ -5,15 +5,17 @@ import useFetchList from "lib/utils/hooks/useFetchList";
 
 const useNewsFeed = () => {
 	const [url, setUrl] = useState(SITE_OPTIONS["frontend-focus"].value);
-	const isFeedUrl = url.includes(".xml");
+	const isFeedItem = Object.values(SITE_OPTIONS).find(
+		(item) => item.isFeedItem && item.value === url
+	);
 
 	const { data, isLoading, isError } = useFetchList(
 		url,
-		isFeedUrl ? `${ROOT_URL}${url}` : url
+		isFeedItem ? `${ROOT_URL}${url}` : url
 	);
 	let items = undefined;
 	if (data) {
-		items = isFeedUrl ? parseXML(data) : data.articles;
+		items = isFeedItem ? parseXML(data) : data.articles;
 	}
 	return { data: items, isLoading, isError, setUrl };
 };
