705980243d84ec262346ebdc73bf2980756eb08c
lifeparticle <mahbubzaman17@gmail.com>
2023-09-21 23:31:01 +1000

    refactor

--- ui/src/components/Hoc/withPageTitle/utils/constants.ts
+++ ui/src/components/Hoc/withPageTitle/utils/constants.ts
@@ -1,9 +1,7 @@
 import { routesById } from "pages/Routes/utils/constant";
 
-const PAGES = ["About", "Feedback", "Newsfeed"];
-
 const HELP = {
 	[routesById.colorpicker.id]: "colorpicker",
 	[routesById.shadesandtints.id]: "shadesandtints",
 };
-export { PAGES, HELP };
+export { HELP };
