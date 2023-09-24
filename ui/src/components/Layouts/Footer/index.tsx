a9749f948b57c46d4144257942a89d2a8924b80b
ashik-75 <ashikex49@gmail.com>
2023-09-24 17:08:25 +0600

    add footer

--- ui/src/components/Layouts/Footer/index.tsx
+++ ui/src/components/Layouts/Footer/index.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import style from "./Footer.module.scss";
 import Icon from "components/General/Icon";
-import { Typography, theme } from "antd";
+import { Col, Row, Space, Typography, theme } from "antd";
 import { ResponsiveButton } from "components/General/FormComponents";
+import { Link } from "react-router-dom";
+import logo_light from "assets/logo_light.svg";
+import logo_dark from "assets/logo_dark.svg";
+import { DarkModeContext } from "lib/utils/context/DarkModeProvider";
 
 const Footer: React.FC = () => {
+	const { isDarkMode } = useContext(DarkModeContext);
 	const {
 		token: { colorBgContainer },
 	} = theme.useToken();
@@ -14,52 +19,120 @@ const Footer: React.FC = () => {
 			className={style.footer}
 			style={{ backgroundColor: colorBgContainer }}
 		>
-			<div className={style.footer__left}>
-				<div className={style.footer__left_year}>
-					<Typography.Text>
-						&copy; {new Date().getFullYear()} Binarytree
-					</Typography.Text>
-				</div>
-				<ul className={style.footer__left_list}>
-					<li>
-						<a
-							href="https://github.com/lifeparticle/binarytree/blob/main/CODE_OF_CONDUCT.md"
-							target="_blank"
-						>
-							Code of Conduct
-						</a>
-					</li>
+			<Row gutter={[120, 16]}>
+				<Col xs={24} lg={6} className={style.footer__left}>
+					<Space direction="vertical" size={"large"}>
+						<Space>
+							<Link to={"/"}>
+								<img
+									src={isDarkMode ? logo_dark : logo_light}
+									height={60}
+									width={60}
+								/>
+							</Link>
+							<Typography.Title level={4}>
+								Binarytree
+							</Typography.Title>
+						</Space>
+						<ResponsiveButton
+							key="Github"
+							type="default"
+							shape="circle"
+							icon={<Icon name="Github" />}
+							onClick={() =>
+								window.open(
+									"https://github.com/lifeparticle/binarytree",
+									"_blank",
+									"noopener"
+								)
+							}
+						/>
 
-					<li>
-						<a href="#">Privacy</a>
-					</li>
-					<li>
-						<a href="#">Terms</a>
-					</li>
-					<li>
-						<a href="https://github.com/lifeparticle/binarytree#status">
-							Status
-						</a>
-					</li>
-					<li>
-						<a href="/about">About</a>
-					</li>
-				</ul>
-			</div>
+						<Typography.Text>
+							&copy; {new Date().getFullYear()} Binarytree
+						</Typography.Text>
+					</Space>
+				</Col>
 
-			<ResponsiveButton
-				key="Github"
-				type="default"
-				shape="circle"
-				icon={<Icon name="Github" />}
-				onClick={() =>
-					window.open(
-						"https://github.com/lifeparticle/binarytree",
-						"_blank",
-						"noopener"
-					)
-				}
-			/>
+				<Col xs={24} lg={18}>
+					<Row gutter={[16, 16]}>
+						<Col xs={24} lg={6}>
+							<Typography.Title level={4}>
+								Product
+							</Typography.Title>
+							<ul className={style.footer__list}>
+								<li>
+									<a
+										href="https://github.com/users/lifeparticle/projects/2"
+										target="_blank"
+									>
+										Roadmap
+									</a>
+								</li>
+							</ul>
+						</Col>
+
+						<Col xs={24} lg={6}>
+							<Typography.Title level={4}>
+								Developers
+							</Typography.Title>
+							<ul className={style.footer__list}>
+								<li>
+									<a
+										href="https://github.com/lifeparticle/binarytree/blob/main/CODE_OF_CONDUCT.md"
+										target="_blank"
+									>
+										Code of Conduct
+									</a>
+								</li>
+								<li>
+									<a
+										href="https://github.com/lifeparticle/binarytree#status"
+										target="_blank"
+									>
+										Status
+									</a>
+								</li>
+							</ul>
+						</Col>
+
+						<Col xs={24} lg={6}>
+							<Typography.Title level={4}>
+								Company
+							</Typography.Title>
+							<ul className={style.footer__list}>
+								<li>
+									<a href="/about">About</a>
+								</li>
+								{/* <li>
+									<a
+										href="#"
+										target="_blank"
+									>
+										Blog
+									</a>
+								</li> */}
+							</ul>
+						</Col>
+
+						<Col xs={24} lg={6}>
+							<Typography.Title level={4}>Legal</Typography.Title>
+							<ul className={style.footer__list}>
+								<li>
+									<a href="#">Terms</a>
+								</li>
+								<li>
+									<a href="#">Privacy</a>
+								</li>
+
+								<li>
+									<a href="/cookie-policy">Cookie Policy</a>
+								</li>
+							</ul>
+						</Col>
+					</Row>
+				</Col>
+			</Row>
 		</div>
 	);
 };
