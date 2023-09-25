d296a787e80d2ecf4e703929a92fe7687e8af3ec
lifeparticle <mahbubzaman17@gmail.com>
2023-09-26 00:39:36 +1000

    refactor
    Former-commit-id: bb7276680bf2d93706da8c6d2486680e81afef0b

--- ui/src/pages/Home/components/Hero.tsx
+++ ui/src/pages/Home/components/Hero.tsx
@@ -1,25 +1,36 @@
 import React from "react";
 import style from "pages/Home/Home.module.scss";
-import { Typography } from "antd";
+import { Typography, Image } from "antd";
 import { FEATURE_DATA } from "pages/About/utils/constants";
-
+import hero from "assets/home/hero.svg";
 const { Title } = Typography;
 const Hero: React.FC = () => {
 	return (
 		<section className={style.home__hero}>
-			<Title>Your Mate in Software Development</Title>
-			<Typography.Paragraph className={style.about__intro_wrapper_text}>
-				At binarytree.dev, we provide an array of developer productivity
-				tools designed to help you save time. With an{" "}
-				<a
-					href="https://github.com/users/lifeparticle/projects/2"
-					target="_blank"
+			<div className={style.home__hero_text}>
+				<Title>Your Mate in Software Development</Title>
+				<Typography.Paragraph
+					className={style.about__intro_wrapper_text}
 				>
-					ever-growing number of features
-				</a>{" "}
-				(currently <b>{FEATURE_DATA.length}</b>), our platform is
-				constantly evolving to meet the needs of developers like you
-			</Typography.Paragraph>
+					At binarytree.dev, we provide an array of developer
+					productivity tools designed to help you save time. With an{" "}
+					<a
+						href="https://github.com/users/lifeparticle/projects/2"
+						target="_blank"
+					>
+						ever-growing number of features
+					</a>{" "}
+					(currently <b>{FEATURE_DATA.length}</b>), our platform is
+					constantly evolving to meet the needs of developers like you
+				</Typography.Paragraph>
+			</div>
+			<Image
+				src={hero}
+				alt="BinaryTree: Developer Productivity Tools"
+				className={style.home__hero_image}
+				height={400}
+				preview={false}
+			/>
 		</section>
 	);
 };
