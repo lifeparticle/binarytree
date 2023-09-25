d8736459efcf3327e585ff734a8f5be167216482
lifeparticle <mahbubzaman17@gmail.com>
2023-09-25 23:01:43 +1000

    refactor footer

--- ui/src/components/Layouts/Footer/index.tsx
+++ ui/src/components/Layouts/Footer/index.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import style from "./Footer.module.scss";
 import Icon from "components/General/Icon";
-import { Space, Typography, theme } from "antd";
+import { Space, Typography } from "antd";
 import { ResponsiveButton } from "components/General/FormComponents";
 import { Link } from "react-router-dom";
 import bt_light from "assets/bt_light.png";
@@ -11,26 +11,25 @@ import { openLink } from "lib/utils/helper";
 
 const Footer: React.FC = () => {
 	const { isDarkMode } = useContext(DarkModeContext);
-	const {
-		token: { colorBgContainer },
-	} = theme.useToken();
 
 	return (
 		<div
 			className={style.footer}
-			style={{ backgroundColor: colorBgContainer }}
+			style={{
+				backgroundColor: isDarkMode ? "#2b2b2b" : "#e6e6e6",
+			}}
 		>
 			<Space
 				direction="vertical"
 				size={"large"}
 				className={style.footer__left}
+				align="center"
 			>
 				<Space>
 					<Link to={"/"}>
 						<img
 							src={isDarkMode ? bt_dark : bt_light}
 							height={100}
-							// width={}
 						/>
 					</Link>
 				</Space>
@@ -43,14 +42,13 @@ const Footer: React.FC = () => {
 					onClick={() =>
 						openLink("https://github.com/lifeparticle/binarytree")
 					}
-					style={{ marginLeft: "20px" }}
 				/>
-				<Typography.Text style={{ marginLeft: "20px" }}>
+				<Typography.Text>
 					&copy; {new Date().getFullYear()} BinaryTree
 				</Typography.Text>
 			</Space>
 
-			<Space direction="vertical">
+			<Space direction="vertical" className={style.footer__right}>
 				<Typography.Title level={4}>Product</Typography.Title>
 				<ul className={style.footer__list}>
 					<li>
@@ -67,7 +65,7 @@ const Footer: React.FC = () => {
 				</ul>
 			</Space>
 
-			<Space direction="vertical">
+			<Space direction="vertical" className={style.footer__right}>
 				<Typography.Title level={4}>Developers</Typography.Title>
 				<ul className={style.footer__list}>
 					<li>
@@ -95,7 +93,7 @@ const Footer: React.FC = () => {
 				</ul>
 			</Space>
 
-			<Space direction="vertical">
+			<Space direction="vertical" className={style.footer__right}>
 				<Typography.Title level={4}>Company</Typography.Title>
 				<ul className={style.footer__list}>
 					<li>
@@ -111,7 +109,7 @@ const Footer: React.FC = () => {
 				</ul>
 			</Space>
 
-			<Space direction="vertical">
+			<Space direction="vertical" className={style.footer__right}>
 				<Typography.Title level={4}>Legal</Typography.Title>
 				<ul className={style.footer__list}>
 					<li>
