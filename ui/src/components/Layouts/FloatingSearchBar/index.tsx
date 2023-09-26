f17d2626bb969e3ff4014f134139736793de6362
lifeparticle <mahbubzaman17@gmail.com>
2023-09-26 23:46:49 +1000

    fix search bar
    Former-commit-id: cc5fe5bb94e9b399d9e18ff6484c361e1e8e99aa

--- ui/src/components/Layouts/FloatingSearchBar/index.tsx
+++ ui/src/components/Layouts/FloatingSearchBar/index.tsx
@@ -1,9 +1,17 @@
-import { Input } from "antd";
+import { Input, theme } from "antd";
 import style from "./FloatingSearchBar.module.scss";
 import Icon from "components/General/Icon";
 const FloatingSearchBar = () => {
+	const {
+		token: { colorBgContainer },
+	} = theme.useToken();
 	return (
-		<div className={style.fsb}>
+		<div
+			className={style.fsb}
+			style={{
+				backgroundColor: colorBgContainer,
+			}}
+		>
 			<Input
 				addonBefore={<Icon name="Search" />}
 				addonAfter={<>cmd + k</>}
