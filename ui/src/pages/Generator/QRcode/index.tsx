995f580bda2cb8d27b87dd0b9fcb74c1599c1052
lifeparticle <mahbubzaman17@gmail.com>
2023-10-13 19:40:16 +1100

    refactor qr code

--- ui/src/pages/Generator/QRcode/index.tsx
+++ ui/src/pages/Generator/QRcode/index.tsx
@@ -1,21 +1,39 @@
 import { Card, Form, QRCode, Input, Badge, Space } from "antd";
 import PageGrid from "components/Layouts/PageGrid";
-import React, { useEffect, useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import { downloadQRCode } from "./utils/helper";
 import style from "./QRcode.module.scss";
 import DropdownDownloadButton from "components/General/DropdownDownloadButton";
 import Warning from "components/General/Warning";
 import { detectData } from "pages/Tools/Sorting/utils/helper";
+import ColorPickerWithInput from "components/General/ColorPickerWithInput";
+import { ResponsiveInputWithLabel } from "components/General/FormComponents";
 
 const { TextArea } = Input;
 
 const QRcode: React.FC = () => {
 	const [value, setValue] = useState("");
 	const [dataType, setDataType] = useState("");
+	const [color, setColor] = useState("#000000");
+	const [bgColor, setBgColor] = useState("#FFFFFF");
+	const [bordered, setBordered] = useState(false);
+	const [icon, setIcon] = useState<string | undefined>(undefined);
+	const [size, setSize] = useState(200);
 
 	useEffect(() => {
 		setDataType(detectData(value));
 	}, [value]);
+
+	const handleImageUpload = (e: ChangeEvent<HTMLInputElement>) => {
+		if (e.target.files && e.target.files.length > 0) {
+			const file = e.target.files[0];
+			const reader = new FileReader();
+			reader.onload = () => {
+				setIcon(reader.result as string);
+			};
+			reader.readAsDataURL(file);
+		}
+	};
 
 	return (
 		<PageGrid>
@@ -44,6 +62,54 @@ const QRcode: React.FC = () => {
 							allowClear
 						/>
 					</Form.Item>
+					<PageGrid>
+						<ColorPickerWithInput
+							value={color}
+							setValue={(e) => setColor(e.target.value)}
+							setColor={setColor}
+							label="Color"
+						/>
+						<ColorPickerWithInput
+							value={bgColor}
+							setValue={(e) => setBgColor(e.target.value)}
+							setColor={setBgColor}
+							label="Color"
+						/>
+					</PageGrid>
+
+					<Form.Item
+						label="Border"
+						valuePropName="checked"
+						tooltip="Add border to QR code"
+					>
+						<Input
+							type="checkbox"
+							onChange={(e) => setBordered(e.target.checked)}
+						/>
+					</Form.Item>
+
+					<Form.Item
+						label="Border"
+						valuePropName="checked"
+						tooltip="Add border to QR code"
+					>
+						<ResponsiveInputWithLabel
+							label="Image height"
+							placeholder="Height"
+							value={size}
+							onChange={(val) => val && setSize(val)}
+							min={0}
+							type="number"
+						/>
+					</Form.Item>
+
+					<Form.Item label="Upload Image">
+						<Input
+							type="file"
+							accept="image/*"
+							onChange={handleImageUpload}
+						/>
+					</Form.Item>
 				</Form>
 			</Card>
 			<Card className={style.qrcode__output}>
@@ -54,7 +120,15 @@ const QRcode: React.FC = () => {
 						size={"large"}
 						id="myqrcode"
 					>
-						<QRCode value={value} />
+						<QRCode
+							value={value}
+							color={color}
+							bgColor={bgColor}
+							bordered={bordered}
+							size={size}
+							iconSize={size / 4}
+							icon={icon}
+						/>
 						<DropdownDownloadButton
 							handleDownload={downloadQRCode}
 						/>
