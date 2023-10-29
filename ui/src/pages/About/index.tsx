eb8e8656c17a8d616b5a796bee1790e554a6051f
lifeparticle <mahbubzaman17@gmail.com>
2023-10-30 02:08:56 +1100

    about page
    Former-commit-id: f4762521b8d53c8e5d9553f3158209a42729aee4

--- ui/src/pages/About/index.tsx
+++ ui/src/pages/About/index.tsx
@@ -10,12 +10,9 @@ const About = () => {
 	return (
 		<>
 			<div className={style.about}>
-				<Card className={style.about__card}>
+				<Card className={style.about__card_cover}>
 					<div className={style.about__card__intro}>
-						<Typography.Title
-							level={5}
-							className={style.about__card__intro_text}
-						>
+						<p className={style.about__card__intro_text}>
 							Hey there, fellow developers! I hope you're having a
 							fantastic day and your code is running smoothly.
 							Don't worry if it's not; we've all been there. I've
@@ -46,9 +43,8 @@ const About = () => {
 							I hope BinaryTree will be a valuable addition to
 							your developer toolkit. Happy coding!
 							<br />
-							<br />
-							<p>{`— A node in the BinaryTree.`}</p>
-						</Typography.Title>
+							<br />— A node in the BinaryTree.
+						</p>
 					</div>
 				</Card>
 				<Card className={style.about__card} bordered={false}>
