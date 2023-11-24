a2385daa755f303c99b674e83e99cf477ac0327c
lifeparticle <mahbubzaman17@gmail.com>
2023-11-25 01:52:08 +1100

    add toml
    Former-commit-id: c75d2fe0a53d10795bb174da2fb4a813b9b05d4f

--- ui/src/pages/Converter/File/useFfmpeg.ts
+++ ui/src/pages/Converter/File/useFfmpeg.ts
@@ -1,22 +1,20 @@
 import { toBlobURL } from "@ffmpeg/util";
 import { useEffect, useReducer, useRef } from "react";
 import { FFmpeg } from "@ffmpeg/ffmpeg";
+
 let loaded = false;
 const ffmpeg = new FFmpeg();
 
 export const useFfmpeg = () => {
-	// const [loaded, setLoaded] = useState(false);
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	const [_, rerender] = useReducer((s) => s + 1, 0);
 	const messageRef = useRef<HTMLParagraphElement | null>(null);
-	// const ffmpegRef = useRef(new FFmpeg());
 
 	const load = async () => {
 		if (loaded) {
 			return;
 		}
-		const baseURL = "https://unpkg.com/@ffmpeg/core-mt@0.12.4/dist/esm";
-		// const ffmpeg = ffmpegRef.current;
+		const baseURL = "";
 		ffmpeg.on("log", ({ message }) => {
 			if (messageRef.current) messageRef.current.innerHTML = message;
 		});
@@ -36,7 +34,6 @@ export const useFfmpeg = () => {
 			),
 		});
 		loaded = true;
-		// setLoaded(true);
 		rerender();
 	};
 
