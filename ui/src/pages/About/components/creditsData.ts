8fa4339f748624740cb27df486825013fde0bd33
lifeparticle <mahbubzaman17@gmail.com>
2023-11-25 12:35:33 +1100

    sonar duplication warning fixed
    Former-commit-id: 6bb9f81a5eac69c312a208281f1d7eac2e68d02b

---
+++ ui/src/pages/About/components/creditsData.ts
@@ -0,0 +1,58 @@
+interface Credits {
+	key: string;
+	name: string;
+	url: string;
+}
+
+export const CREDITS_DATA: Credits[] = [
+	{
+		key: "1",
+		name: "thetallbloke",
+		url: "https://github.com/thetallbloke",
+	},
+	{
+		key: "2",
+		name: "Open Props",
+		url: "https://github.com/argyleink/open-props",
+	},
+	{
+		key: "3",
+		name: "News API",
+		url: "https://newsapi.org",
+	},
+	{
+		key: "4",
+		name: "Movie for hackers",
+		url: "https://github.com/k4m4/movies-for-hackers",
+	},
+	{
+		key: "5",
+		name: "unDraw",
+		url: "https://undraw.co/",
+	},
+	{
+		key: "6",
+		name: "Frontend Focus",
+		url: "https://frontendfoc.us/",
+	},
+	{
+		key: "7",
+		name: "React Status",
+		url: "https://react.statuscode.com/",
+	},
+	{
+		key: "8",
+		name: "Lucide",
+		url: "https://lucide.dev/",
+	},
+	{
+		key: "9",
+		name: "Dudeowl",
+		url: "https://99designs.com.au/profiles/dudeowl",
+	},
+	{
+		key: "10",
+		name: "Vertex Designs",
+		url: "https://unsplash.com/@vertex_800",
+	},
+];
--- ui/src/utils/helper-classes/__tests__/DataDetection.test.ts
+++ ui/src/utils/helper-classes/__tests__/DataDetection.test.ts
@@ -1,78 +1,56 @@
-import { DataDetection } from "utils/helper-classes/DataDetection";
+import { DataDetection, DataType } from "utils/helper-classes/DataDetection";
+
+describe("DataDetection", () => {
+	const testData = [
+		{ types: ["string"], data: "This is a string", expected: "string" },
+		{ types: ["url"], data: "https://binarytree.dev/", expected: "url" },
+		{ types: ["number"], data: "123", expected: "number" },
+		{ types: ["array"], data: "[1,2,3]", expected: "array" },
+		{ types: ["object"], data: '{"key": "value"}', expected: "object" },
+		{ types: ["boolean"], data: "true", expected: "boolean" },
+		{
+			types: ["number"],
+			data: "Unsupported data",
+			expected: "Can't detect data",
+		},
+		{
+			types: ["number", "string"],
+			data: "123, Some string",
+			expected: "string",
+			isMultiple: true,
+		},
+		{
+			types: ["number", "string"],
+			data: "1, 2",
+			expected: "number",
+			isMultiple: true,
+		},
+		{
+			types: ["number", "string", "array"],
+			data: "[1, 2]",
+			expected: "array",
+			isMultiple: true,
+		},
+		{
+			types: ["number", "string", "array"],
+			data: '["1", "2"]',
+			expected: "array",
+			isMultiple: true,
+		},
+	];
+
+	testData.forEach(({ types, data, expected, isMultiple = false }) => {
+		test(`should detect "${expected}" type correctly`, () => {
+			const detector = new DataDetection(new Set(types as DataType[]));
+			detector.setData(data, isMultiple);
+			expect(detector.detect()).toBe(expected);
+		});
+	});
+});
 
 describe("DataDetection", () => {
 	test('should detect "No data" when data is null', () => {
 		const detector = new DataDetection(new Set(["string"]));
 		expect(detector.detect()).toBe("No data");
-	});
-
-	test('should detect "url" type correctly', () => {
-		const detector = new DataDetection(new Set(["url"]));
-		detector.setData("https://binarytree.dev/");
-		expect(detector.detect()).toBe("url");
-	});
-
-	test('should detect "string" type correctly', () => {
-		const detector = new DataDetection(new Set(["string"]));
-		detector.setData("This is a string");
-		expect(detector.detect()).toBe("string");
-	});
-
-	test('should detect "number" type correctly', () => {
-		const detector = new DataDetection(new Set(["number"]));
-		detector.setData("123");
-		expect(detector.detect()).toBe("number");
-	});
-
-	test('should detect "array" type when JSON string is provided', () => {
-		const detector = new DataDetection(new Set(["array"]));
-		detector.setData("[1,2,3]");
-		expect(detector.detect()).toBe("array");
-	});
-
-	test('should detect "object" type when JSON object string is provided', () => {
-		const detector = new DataDetection(new Set(["object"]));
-		detector.setData('{"key": "value"}');
-		expect(detector.detect()).toBe("object");
-	});
-
-	test('should detect "boolean" type correctly', () => {
-		const detector = new DataDetection(new Set(["boolean"]));
-		detector.setData("true");
-		expect(detector.detect()).toBe("boolean");
-	});
-
-	test('should return "Can\'t detect data" for unsupported types', () => {
-		const detector = new DataDetection(new Set(["number"]));
-		detector.setData("Unsupported type");
-		expect(detector.detect()).toBe("Can't detect data");
-	});
-
-	test("should detect string multiple data types when isMultiple is true", () => {
-		const detector = new DataDetection(new Set(["number", "string"]));
-		detector.setData("123, Some string", true);
-		expect(detector.detect()).toBe("string");
-	});
-
-	test("should detect number multiple data types when isMultiple is true", () => {
-		const detector = new DataDetection(new Set(["number", "string"]));
-		detector.setData("1, 2", true);
-		expect(detector.detect()).toBe("number");
-	});
-
-	test("should detect array of numbers multiple data types when isMultiple is true", () => {
-		const detector = new DataDetection(
-			new Set(["number", "string", "array"])
-		);
-		detector.setData("[1, 2]", true);
-		expect(detector.detect()).toBe("array");
-	});
-
-	test("should detect array of strings multiple data types when isMultiple is true", () => {
-		const detector = new DataDetection(
-			new Set(["number", "string", "array"])
-		);
-		detector.setData('["1", "2"]', true);
-		expect(detector.detect()).toBe("array");
 	});
 });
