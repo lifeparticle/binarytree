cfe275b0e80ce0a22bb02677b48202373e46700d
lifeparticle <mahbubzaman17@gmail.com>
2023-12-30 21:02:33 +1100

    fix sonar cloud issue
    Former-commit-id: 4d627f01d07591a488a3375b45addf26f718e404

--- ui/src/pages/Feedback/Feedback.tsx
+++ ui/src/pages/Feedback/Feedback.tsx
@@ -31,6 +31,7 @@ const AirtableEmbed = () => {
 					src="https://airtable.com/embed/appbcH1EyjqD8PXkK/shrm5RlUHdworbgSz?backgroundColor=blue"
 					height="1949.375"
 					onLoad={() => setLoading(false)}
+					role="presentation"
 				></iframe>
 			</div>
 			<Footer />
