45c430bdf7a87e985ca0de21e1ed4a09179ee0f4
lifeparticle <mahbubzaman17@gmail.com>
2024-01-03 22:44:38 +1100

    handle win
    Former-commit-id: ad30d8845d20c7c709e585ea08fb4a2cd01f99e3

--- ui/postinstall.js
+++ ui/postinstall.js
@@ -1,11 +1,23 @@
 import fse from "fs-extra";
 import path from "path";
 
-const topDir = path.dirname(new URL(import.meta.url).pathname);
+try {
+	const urlPath = new URL(import.meta.url).pathname;
+	// Correcting the path for Windows environment
+	const topDir = path.resolve(
+		process.platform === "win32" ? urlPath.substring(1) : urlPath
+	);
 
-fse.emptyDirSync(path.join(topDir, "public", "tinymce"));
-fse.copySync(
-	path.join(topDir, "node_modules", "tinymce"),
-	path.join(topDir, "public", "tinymce"),
-	{ overwrite: true }
-);
+	const sourceDir = path.join(topDir, "node_modules", "tinymce");
+	const destDir = path.join(topDir, "public", "tinymce");
+
+	// Empty the destination directory
+	fse.emptyDirSync(destDir);
+
+	// Copy files from source to destination
+	fse.copySync(sourceDir, destDir, { overwrite: true });
+
+	console.log("Copy operation completed successfully.");
+} catch (error) {
+	console.error("Error during file operation:", error);
+}
