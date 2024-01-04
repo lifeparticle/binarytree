4256437b8be10373698c1536203e51b5d25cadbb
lifeparticle <mahbubzaman17@gmail.com>
2024-01-04 21:04:35 +1100

    fix build

--- ui/src/pages/Home/components/Hero.tsx
+++ ui/src/pages/Home/components/Hero.tsx
@@ -2,7 +2,6 @@ import { FC } from "react";
 import style from "pages/Home/Home.module.scss";
 import { Typography, Image, Space, Card } from "antd";
 import hero from "assets/Home/hero.webp";
-import { FEATURE_DATA } from "data/featureData";
 import { ResponsiveButton } from "components/General";
 import { useModal } from "hooks";
 import { STATS_DATA } from "pages/Home/statsData";
