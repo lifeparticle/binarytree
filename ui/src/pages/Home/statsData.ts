5e82c6e37f864724d48b92c013525cc756febfaa
lifeparticle <mahbubzaman17@gmail.com>
2024-01-04 21:00:25 +1100

    update home page
    Former-commit-id: 730809f86c990fa396437f46161dd14d617e3da2

---
+++ ui/src/pages/Home/statsData.ts
@@ -0,0 +1,20 @@
+export const STATS_DATA = [
+	{
+		id: 1,
+		title: "Features",
+		value: 28,
+		link: "",
+	},
+	{
+		id: 2,
+		title: "GitHub Stars",
+		value: 22,
+		link: "https://github.com/lifeparticle/binarytree/stargazers",
+	},
+	{
+		id: 3,
+		title: "Contributors",
+		value: 6,
+		link: "https://github.com/lifeparticle/binarytree/graphs/contributors",
+	},
+];
