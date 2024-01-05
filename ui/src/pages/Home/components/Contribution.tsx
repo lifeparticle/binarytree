4ac17f953fddee3ad1c6e7d2c7080def6b12356e
lifeparticle <mahbubzaman17@gmail.com>
2024-01-05 21:22:09 +1100

    fix home page ui
    Former-commit-id: 7620d3d5024a270355a23fe519d8f6d7840e9964

--- ui/src/pages/Home/components/Contribution.tsx
+++ ui/src/pages/Home/components/Contribution.tsx
@@ -14,64 +14,60 @@ const Contribution: FC = () => {
 
 	return (
 		<section className={style.home__contribution}>
-			<Space direction="vertical">
-				<Image
-					src={features}
-					alt="BinaryTree: Developer Productivity Tools"
-					preview={false}
-					height="25vh"
-				/>
-				<div className={style.home__features_cover}>
-					<Title level={3}>
-						BinaryTree comes packed with some awesome features. For
-						modern app development, tools like BinaryTree can
-						significantly enhance and streamline your workflow.
-					</Title>
-				</div>
-				<div className={style.home__hero__stats_container}>
-					{STATS_DATA.map((feature) => (
-						<Card
-							bordered={false}
-							key={feature.id}
-							hoverable
-							onClick={() =>
-								feature.link === ""
-									? handleModalOpen()
-									: window.open(
-											feature.link,
-											"_blank",
-											"noopener"
-									  )
-							}
-						>
-							<Space direction="vertical" size="middle">
-								<Title keyboard>{feature.value}</Title>
-								<Text strong>{feature.title}</Text>
-							</Space>
-						</Card>
-					))}
-				</div>
-				<Typography.Title level={2}>
-					Join our community
-				</Typography.Title>
-				<Typography.Paragraph>
-					Join our open-source community and help shape the future of
-					Modern web
-				</Typography.Paragraph>
-				<Space direction="horizontal">
-					<ResponsiveButton
-						className="button-right-icon"
+			<Image
+				src={features}
+				alt="BinaryTree: Developer Productivity Tools"
+				preview={false}
+				height="25vh"
+			/>
+			<div className={style.home__features_cover}>
+				<Title level={3}>
+					BinaryTree comes packed with some awesome features. For
+					modern app development, tools like BinaryTree can
+					significantly enhance and streamline your workflow.
+				</Title>
+			</div>
+			<div className={style.home__contribution__stats_container}>
+				{STATS_DATA.map((feature) => (
+					<Card
+						bordered={false}
+						key={feature.id}
+						hoverable
 						onClick={() =>
-							window.open(
-								"https://github.com/lifeparticle/binarytree",
-								"_blank",
-								"noopener"
-							)
+							feature.link === ""
+								? handleModalOpen()
+								: window.open(
+										feature.link,
+										"_blank",
+										"noopener"
+								  )
 						}
 					>
-						Github Discussion <Icon name="MoveUpRight" />
-					</ResponsiveButton>
-				</Space>
+						<Space direction="vertical" size="middle">
+							<Title keyboard>{feature.value}</Title>
+							<Text strong>{feature.title}</Text>
+						</Space>
+					</Card>
+				))}
+			</div>
+			<Typography.Title level={2}>Join our community</Typography.Title>
+			<Typography.Paragraph>
+				Join our open-source community and help shape the future of
+				Modern web
+			</Typography.Paragraph>
+			<Space direction="horizontal">
+				<ResponsiveButton
+					className="button-right-icon"
+					onClick={() =>
+						window.open(
+							"https://github.com/lifeparticle/binarytree",
+							"_blank",
+							"noopener"
+						)
+					}
+				>
+					Github Discussion <Icon name="MoveUpRight" />
+				</ResponsiveButton>
 			</Space>
 		</section>
 	);
