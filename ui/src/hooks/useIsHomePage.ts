46786e03ba34df08a9582daa9668e20f7224d131
lifeparticle <mahbubzaman17@gmail.com>
2024-01-07 20:36:44 +1100

    tidy
    Former-commit-id: 011e3152d5899910a39511c993bbe084e08c727b

--- ui/src/hooks/useIsHomePage.ts
+++
@@ -1,9 +0,0 @@
-import { useLocation } from "react-router-dom";
-
-const useIsHomePage = () => {
-	const location = useLocation();
-	const isHomePage = location.pathname !== "/";
-	return isHomePage;
-};
-
-export default useIsHomePage;
