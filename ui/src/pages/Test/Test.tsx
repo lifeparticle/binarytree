9da1f653b8d418be804349459636eac9dc5d9eac
lifeparticle <mahbubzaman17@gmail.com>
2024-01-07 20:46:44 +1100

    fix sonar cloud issue
    Former-commit-id: 44dbf2482d89f11523ea5145597d748d8b388b6d

--- ui/src/pages/Test/Test.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from "react";
-import style from "./Test.module.scss";
-
-const Test : FC = () => {
-	return <div className={style.test}>Test</div>;
-};
-
-export default Test;
--- ui/src/pages/Test/__test__/Test.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import Test from "pages/Test";
-
-describe("Test Component", () => {
-	it("renders correctly", () => {
-		const { getByText } = render(<Test />);
-		expect(getByText("Test")).toBeInTheDocument();
-	});
-});
