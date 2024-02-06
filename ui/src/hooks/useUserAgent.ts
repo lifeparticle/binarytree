4d76fcccdb52ecac2092a9d8c2b85096873db9a1
lifeparticle <mahbubzaman17@gmail.com>
2024-02-06 23:15:12 +1100

    new ui pr workflow
    Former-commit-id: 9dce14673c8585ec18402af869929cc14ed256f6

--- ui/src/hooks/useUserAgent.ts
+++ ui/src/hooks/useUserAgent.ts
@@ -1,5 +1,5 @@
 export default function useUserAgent() {
 	const userAgent = navigator.userAgent.toLowerCase();
 
-	return userAgent.indexOf("win") != -1 ? "win" : "mac";
+	return userAgent.indexOf("win") !== -1 ? "win" : "mac";
 }
