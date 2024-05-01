5ced5f92353523cbf0a448468937cfecb2cd0aec
lifeparticle <mahbubzaman17@gmail.com>
2024-05-01 22:59:19 +1000

    update

--- ui/src/pages/Generator/Uuid/Uuid.tsx
+++ ui/src/pages/Generator/Uuid/Uuid.tsx
@@ -1,6 +1,6 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import style from "./Uuid.module.scss";
-import { Card, Typography } from "antd";
+import { Card, Typography, InputNumber, List } from "antd";
 import { Clipboard } from "components/ComponentInjector";
 import { ClipboardButton } from "components/InjectedComponent";
 import { Icon, ResponsiveButton } from "components/General";
@@ -8,21 +8,58 @@ import { Icon, ResponsiveButton } from "components/General";
 const { Title } = Typography;
 
 const Uuid: FC = () => {
-	const [uuid, setUuid] = useState(crypto.randomUUID());
+	const [uuids, setUuids] = useState([crypto.randomUUID()]);
+	const [numUuids, setNumUuids] = useState(1);
+
+	const generateUuids = (count: number) => {
+		const newUuids = Array.from({ length: count }, () =>
+			crypto.randomUUID()
+		);
+		setUuids(newUuids);
+	};
+
+	useEffect(() => {
+		generateUuids(numUuids);
+	}, [numUuids]);
+
 	return (
 		<div className={style.uuid}>
-			<Card bordered={false} title="UUID">
-				<div className={style.uuid__output}>
-					<Title>{uuid}</Title>
-					<Clipboard
-						text={uuid}
-						clipboardComponent={ClipboardButton}
-					/>
-					<ResponsiveButton
-						icon={<Icon name="RefreshCcw" />}
-						onClick={() => setUuid(crypto.randomUUID())}
-					/>
-				</div>
+			<Card
+				style={{ width: "100%" }}
+				bordered={false}
+				title="UUID Generator"
+				extra={
+					<div className={style.uuid__extra}>
+						<InputNumber
+							min={1}
+							max={100}
+							defaultValue={1}
+							onChange={(value) => value && setNumUuids(value)}
+						/>
+						<ResponsiveButton
+							icon={<Icon name="RefreshCcw" />}
+							onClick={() => generateUuids(numUuids)}
+						/>
+						<Clipboard
+							text={Object.values(uuids).join("\n")}
+							label="Copy All"
+							clipboardComponent={ClipboardButton}
+						/>
+					</div>
+				}
+			>
+				<List
+					dataSource={uuids}
+					renderItem={(uuid) => (
+						<List.Item>
+							<Title level={5}>{uuid}</Title>
+							<Clipboard
+								text={uuid}
+								clipboardComponent={ClipboardButton}
+							/>
+						</List.Item>
+					)}
+				/>
 			</Card>
 		</div>
 	);
